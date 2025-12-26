052842b90a8ae05b44fbb783c32e8b4f1fb0fd1b
msayracode <msayracode@gmail.com>
2025-12-26 14:44:15 +0300

    project app created

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses',
+    
 ]
 
 MIDDLEWARE = [
