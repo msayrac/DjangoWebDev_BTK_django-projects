b4ed3baf0279cebaa92fb6409e279fc03ec26dd5
msayracode <msayracode@gmail.com>
2025-12-27 12:14:19 +0300

    Urls and Views

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+#  http://127.0.0.1:8000/ => anasayfa
+#  http://127.0.0.1:8000/home => anasayfa
+#  http://127.0.0.1:8000/kurslar => kurs listesi
+
+def home(request):
+    return HttpResponse("anasayfa")
+
+def kurslar(request):
+    return HttpResponse("kurs listesi")
 urlpatterns = [
+    path('',home),
+    path('anasayfa',home),
+    path('kurslar',kurslar),
     path('admin/', admin.site.urls),
 ]
