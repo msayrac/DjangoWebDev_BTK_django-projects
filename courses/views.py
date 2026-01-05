de7dc1a8ecbd3d4962914bce8c66fc1bfada7745
msayracode <msayracode@gmail.com>
2026-01-05 18:08:13 +0300

    html tags

--- courses/views.py
+++ courses/views.py
@@ -12,7 +12,16 @@ data ={
 # eklenen metodlar view olarak adlandırılıyoru
 
 def kurslar(request):
-    return HttpResponse("kurs listesi")
+    list_items = ""
+    category_list = list(data.keys())
+
+    for category in category_list:
+        redirect_url = reverse('courses_by_category',args=[category])
+        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>"
+    
+    html = f"<h1>Kurslar listesi</h1><br><ul>{list_items}</ul>"
+
+    return HttpResponse(html)
 
 def details(request,kurs_adi):
     return HttpResponse(f"{kurs_adi} kursu detay sayfası")
@@ -22,7 +31,7 @@ def getCoursesByCategory(request,category_name):
         category_text = data[category_name]
         return HttpResponse(category_text)
     except:
-        return HttpResponseNotFound("yanlış kategori seçimi")
+        return HttpResponseNotFound("<h1>yanlış kategori seçimi</h1>")
 
 def getCoursesByCategoryId(request,category_id):    
     # return HttpResponseRedirect('/kurs/kategori/programlama')
