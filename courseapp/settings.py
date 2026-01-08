e061025dd439c1daa1a9596e56de0ec48904870e
msayracode <msayracode@gmail.com>
2026-01-08 14:46:26 +0300

    layouts are covered

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -57,7 +57,10 @@ ROOT_URLCONF = 'courseapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+           
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
