4102947c78871f003422b0da3418fd1a10b82dd6
msayracode <msayracode@gmail.com>
2026-01-23 14:59:26 +0300

    course dtail page

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
 from datetime import date,datetime
-from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.http import Http404, HttpResponseNotFound
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from courses.models import Course,Category
 
@@ -54,8 +54,21 @@ def index(request):
         'courses':kurslar
     })
 
-def details(request,kurs_adi):
-    return HttpResponse(f"{kurs_adi} kursu detay sayfası")
+def details(request,kurs_id):
+
+    # try:
+    #     course = Course.objects.get(pk=kurs_id)
+    # except:
+    #     raise Http404()
+
+    course = get_object_or_404(Course,pk=kurs_id)
+    
+    context = {
+            'course':course        
+        }
+    return render(request,'courses/details.html',context)
+    
+    
 
 def getCoursesByCategory(request,category_name):
     try:
