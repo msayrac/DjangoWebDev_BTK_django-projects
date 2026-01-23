baa76c90551947a16dc3c8fcdc7166364a56ca39
msayracode <msayracode@gmail.com>
2026-01-23 15:30:31 +0300

    slug field

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 class Course(models.Model):
@@ -8,6 +8,11 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date = models.DateField()
     isActive = models.BooleanField()
+    slug = models.SlugField(default="", null=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(args,kwargs)
 
 
     def __str__(self):
