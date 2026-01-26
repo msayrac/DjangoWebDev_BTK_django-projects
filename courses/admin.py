a7949eda8bbfa8b1eef2c519df063fee4eeb2a87
msayracode <msayracode@gmail.com>
2026-01-26 12:40:22 +0300

    Admin panel in django

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from courses.models import Course,Category
+
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Category)
+
+
+
+
--- courses/models.py
+++ courses/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date = models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default="", null=False, unique = True, db_index=True)
+    slug = models.SlugField(default="", blank="", null=False, unique = True, db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -16,7 +16,7 @@ class Course(models.Model):
 
 
     def __str__(self):
-        return f"{self.title} {self.date}"
+        return f"{self.title}"
 
 
 class Category(models.Model):
