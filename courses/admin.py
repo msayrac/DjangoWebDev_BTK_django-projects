96f914abd8dc30678f93337e079da7c94a7e9e44
msayracode <msayracode@gmail.com>
2026-01-26 12:49:20 +0300

    admin panel features

--- courses/admin.py
+++ courses/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from courses.models import Course,Category
 
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title","isActive","slug")
+    link_display_links = ("title","slug",)
+    readonly_fields = ("slug",)
+    list_filter = ("title","isActive",)
+    list_editable = ("isActive",)
+    search_fields = ("title","description",)
+
 
 # Register your models here.
-admin.site.register(Course)
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
 
 
 
