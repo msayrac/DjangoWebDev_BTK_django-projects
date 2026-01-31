32f624647ac0a3768501680309d963829bd98215
msayracode <msayracode@gmail.com>
2026-01-31 19:33:46 +0300

    form properties

--- courses/forms.py
+++ courses/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(label="Kurs Başlığı", required = True, error_messages = {
+        "required": " kurs başlığı girmelisiniz."})
     description = forms.CharField(widget = forms.Textarea)
     imageUrl = forms.CharField()
     slug = forms.SlugField()
